ac9a0898f6b96dc90b9468f91c053fd7820543fb
yunji0387 <yunji0387@gmail.com>
2023-07-10 17:08:27 -0500

    Add logos to header

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+import ShieldLogo from '../assets/images/miniShield.png';
+import SwordLogo from '../assets/images/miniSword.png';
 
 const Header = () => {
   return (
     <div className="header">
+      <img src={ShieldLogo} alt="Shield Logo" className="header-img-size" />
       <h1 className="header-text">Mini Games Web</h1>
+      <img src={SwordLogo} alt="Shield Logo" className="header-img-size" />
     </div>
   );
 };
