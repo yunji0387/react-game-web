2be63febe26a7441d6770377bc5cbac17b7ee48f
yunji0387 <yunji0387@gmail.com>
2023-07-10 00:46:11 -0500

    Refactor home page

---
+++ src/assets/gameList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import tictactoe from './images/tictactoe.png';
+
+const games = [
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    },
+    {
+        title: 'Tic Tac Toe',
+        description: 'The classic 3 x 3 Tic Tac Toe',
+        imageUrl: tictactoe,
+        imgTitle: 'tictactoe',
+    }
+];
+
+export default games;
