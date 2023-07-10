c0e08db95d289c2f68a9f361af9b0a917403222d
yunji0387 <yunji0387@gmail.com>
2023-07-10 13:12:58 -0500

    Add link to gameCard

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './GameCard.css';
 
 const GameCard = (props) => {
   return (
-    <div className="game-card">
+    <Link to='/about' className="game-card">
       <img className="game-card__image" src={props.imageUrl} alt={props.imgTitle} />
       <div className="game-card__details">
         <h2 className="game-card__title">{props.title}</h2>
         <p className="game-card__description">{props.description}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
