711336c415d3384cda26dca8a5c7ff2abcf3a031
yunji0387 <yunji0387@gmail.com>
2023-07-16 22:15:34 -0500

    refactor GameCard.js

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import './GameCard.css';
 
 const GameCard = (props) => {
+  const WrapperComponent = props.isAvailable ? Link : 'div';
   return (
-    <Link to={props.url} className={`${props.isAvailable ? 'game-card' : 'unavailable-game-card '}`}>
+    <WrapperComponent to={props.url} className={`${props.isAvailable ? 'game-card' : 'unavailable-game-card'}`}>
       <img className="game-card__image" src={props.imageUrl} alt={props.imgTitle} />
       <div className="game-card__details">
         <h2 className="game-card__title">{props.title}</h2>
         <p className="game-card__description">{props.description}</p>
       </div>
-    </Link>
+    </WrapperComponent>
   );
+
 };
 
 export default GameCard;
