65cad43d83401c05310b1077fa2477b0eb2736ed
yunji0387 <yunji0387@gmail.com>
2023-08-25 18:46:13 -0500

    code test

--- src/index.js
+++ src/index.js
@@ -2,21 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createHashRouter,
-  RouterProvider
-} from 'react-router-dom';
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
